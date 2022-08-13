6165b59226d8b8745654935fa37f4d1b0e58eea9
Edwin Alexander Cardenas Esteban <eacardenase@unal.edu.co>
2022-08-13 09:47:21 -0500

    initial setup

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import 'reflect-metadata';
+
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cors());
+
+app.listen(3000, () => {
+    console.log(`Server is listening on port: http://localhost:3000`);
+});
